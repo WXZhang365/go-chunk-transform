e94de24d5dcef4e8b85732e6bada7d7b94c0884a
张文轩 <zhangwenxuan@ghgame.cn>
2025-02-21 11:46:42 +0800

    build: zip release file

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,3 +1,9 @@
-GOOS=windows GOARCH=amd64 go build -o ./dist/gctu_win_amd64.exe
-GOOS=linux GOARCH=amd64 go build -o ./dist/gctu_linux_amd64
-GOOS=linux GOARCH=arm64 go build -o ./dist/gctu_linux_arm64
+GOOS=windows GOARCH=amd64 go build -o ./dist/gctu.exe
+zip ./dist/gctu_win_amd64.zip ./dist/gctu.exe
+rm ./dist/gctu.exe
+GOOS=linux GOARCH=amd64 go build -o ./dist/gctu
+zip ./dist/gctu_linux_amd64.zip ./dist/gctu
+rm ./dist/gctu
+GOOS=linux GOARCH=arm64 go build -o ./dist/gctu
+zip ./dist/gctu_linux_arm64.zip ./dist/gctu
+rm ./dist/gctu
