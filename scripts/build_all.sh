0ddbb163d50af97788d6777cfc50627ed8eed92c
张文轩 <zhangwenxuan@ghgame.cn>
2025-02-21 17:32:37 +0800

    fix: 部分平台CGO问题

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,9 +1,12 @@
-GOOS=windows GOARCH=amd64 go build -o ./dist/gctu.exe
-zip ./dist/gctu_win_amd64.zip ./dist/gctu.exe
+#! /bin/bash
+
+mkdir -p dist
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o ./dist/gctu.exe
+zip -j ./dist/gctu_win_amd64.zip ./dist/gctu.exe
 rm ./dist/gctu.exe
-GOOS=linux GOARCH=amd64 go build -o ./dist/gctu
-zip ./dist/gctu_linux_amd64.zip ./dist/gctu
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ./dist/gctu
+zip -j ./dist/gctu_linux_amd64.zip ./dist/gctu
 rm ./dist/gctu
-GOOS=linux GOARCH=arm64 go build -o ./dist/gctu
-zip ./dist/gctu_linux_arm64.zip ./dist/gctu
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o ./dist/gctu
+zip -j ./dist/gctu_linux_arm64.zip ./dist/gctu
 rm ./dist/gctu
